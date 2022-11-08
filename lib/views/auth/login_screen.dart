3c852e99954ae6f4842d3c2a7bcf03e1306596b8
Jakub Figat <jakub.figat@gmail.com>
2022-11-08 18:53:35 +0100

    11

--- lib/views/auth/login_screen.dart
+++ lib/views/auth/login_screen.dart
@@ -22,7 +22,7 @@ class LoginScreen extends StatelessWidget {
             padding: const EdgeInsets.all(10.0),
             child: TextFormField(
               decoration: InputDecoration(
-                hintText: 'Email Address xems',
+                hintText: 'Email Address',
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
